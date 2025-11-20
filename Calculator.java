6692445de6a0105870300663101b8940e6db35d8
CH-NaveenKumar-2006 <vasusai6281@gmail.com>
2025-11-20 11:17:42 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,12 +2,7 @@ public class Calculator {
     public static void main(String args[]) {
         int a = 10;
         int b=10;
-        System.out.println("addition= " ,(a  + b));
- System.out.println("subtraction= ", (a  - b));
-
- System.out.println("multiplication = " ,(a *b));
-
-      
+        System.out.println("addition= " ,(a  + b)) ;
     }
 }
 
