2f011c62f7c025081e01fbd7e0e23d553d9008a0
CH-NaveenKumar-2006 <vasusai6281@gmail.com>
2025-11-20 11:22:41 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,7 @@
 public class Calculator {
     public static void main(String args[]) {
-        int a = 10;
-        int b=10;
-        System.out.println("addition=", (a+b));
+        int a = 10 , b=20;
+        System.out.println(a+b);
     }
 }
 
