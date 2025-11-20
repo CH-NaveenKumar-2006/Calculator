30cdcd558befe60fbb0528a0666fb50bcd0d0f9a
sreshtakavvampelli <kavvampellisreshta11@gmail.com>
2025-11-20 11:09:06 +0530

    intial commit

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String args[]) {
+        int a = 10;
+        int b=10;
+        System.out.println("addition= " (a  + b));
+ System.out.println("subtraction= " (a  - b));
+
+ System.out.println("multiplication = " (a *b));
+
+      
+    }
+}
+
